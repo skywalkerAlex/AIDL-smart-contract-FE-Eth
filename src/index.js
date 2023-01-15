3946cc28f53c21d340eb92dabe9f2db2b1e14188
skywalkerAlex <alexdim.liak@gmail.com>
2023-01-15 19:13:37 +0200

    fixes during clone

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*!
 
 =========================================================
@@ -19,7 +18,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import {HashRouter, Route, Switch, Redirect} from "react-router-dom";
 import AdminLayout from "layouts/Admin.js";
 // import AuthLayout from "layouts/Auth.js";
 // import RTLLayout from "layouts/RTL.js";
@@ -34,23 +33,4 @@ ReactDOM.render(
     </Switch>
   </HashRouter>,
   document.getElementById("root")
-  );
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> main
